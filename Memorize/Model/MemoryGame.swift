481ba072f95f3376f9b02f246ecf1224b2fa9730
AndrewGHarrington <aghdesigns@gmail.com>
2023-04-23 10:15:32 -0700

    Clean up code in MemoryGame

--- Memorize/Model/MemoryGame.swift
+++ Memorize/Model/MemoryGame.swift
@@ -13,33 +13,29 @@ struct MemoryGame<CardContent> where CardContent: Equatable {
     private var addToScoreAmount = 2
     private var subtractFromScoreAmount = 1
     private var indexOfOneAndOnlyFaceUpCard: Int? {
+        // .oneAndOnly is defined in the extension of Array at the bottom of this file
+        // it looks at an array and if it has only one element in it it returns that element (nil if more than one element)
         get { cards.indices.filter { cards[$0].isFaceUp }.oneAndOnly }
-        set {
-            cards.indices.forEach { cards[$0].isFaceUp = ($0 == newValue) }
-        }
+        set { cards.indices.forEach { cards[$0].isFaceUp = ($0 == newValue) } }
     }
     
     mutating func choose(card: Card<CardContent>) {
-        // TODO: Update code like in lesson 5
         if let chosenIndex = cards.firstIndex(where: { $0.id == card.id }) {
             if indexOfOneAndOnlyFaceUpCard == nil {
                 // no cards flipped over right now
                 indexOfOneAndOnlyFaceUpCard = chosenIndex
-                
-                // turn over card just selected
                 cards[chosenIndex].hasBeenSeen = true
             } else {
                 // at least one card has been flipped over
                 // make sure the currently selected card is not a card that that was just flipped before this one
-                
                 if !(cards[indexOfOneAndOnlyFaceUpCard!].id == card.id) {
                     // check for a match
                     if cards[indexOfOneAndOnlyFaceUpCard!].content == cards[chosenIndex].content {
                         cards[indexOfOneAndOnlyFaceUpCard!].isMatched = true
                         cards[chosenIndex].isMatched = true
-                        
                         score += addToScoreAmount
                     } else {
+                        // if the chosen cards have been seen already, need to deduct from score
                         if cards[indexOfOneAndOnlyFaceUpCard!].hasBeenSeen && cards[chosenIndex].hasBeenSeen {
                             score -= subtractFromScoreAmount
                         }
